1234741294e14b2fdcef5f5075b438b4de27e429
John Doe <webdevnorth@gmail.com>
2016-11-26 21:30:40 +0000

    established client directory structure, boilerplate index.js/html

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('./routes/client_routes.js');
+require('./styles/client_styles.scss')
+
+ReactDOM.render(
+	routes,
+	document.getElementById('app')	
+);
