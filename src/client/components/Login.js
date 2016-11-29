2248ebb188b9654dadaab271db96cc44bb1d08bf
John Doe <webdevnorth@gmail.com>
2016-11-29 16:35:47 +0000

    implemented login, structured and styled for xl-screens

---
+++ src/client/components/Login.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function Login(props) {
+	return (
+		<div>
+			<div className="login-bg"></div>	
+			<h1 className="form-header">Login</h1>	
+			<form action="/login" method="POST">
+				<div className="form-group">
+					<label htmlFor="login-username">Username:</label>
+	    		<input type="text" className="form-control" id="login-username" />
+				</div>
+				<div className="form-group">
+			    <label htmlFor="login-password">Password:</label>
+			    <input type="password" className="form-control" id="login-password" />
+			  </div>
+				<button type="submit" className="btn btn-default form-submit">Log In</button>
+			</form>
+		</div>
+	)
+}
+
+module.exports = Login;
