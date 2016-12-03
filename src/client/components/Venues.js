198559fd2ff5641be51d8aec23e7d756833fd202
John Doe <webdevnorth@gmail.com>
2016-12-03 23:48:48 +0000

    implemented search functionality within venues component

--- src/client/components/Venues.js
+++ src/client/components/Venues.js
@@ -12,8 +12,8 @@ function Venues(props) {
 		<div className="venues-container">	
 			<div className="venues-bg"></div>		
 			<div className="venues-input-container">
-				<input className="venues-search" placeholder="Check another location" type="text" />
-				<div className="search-icon">&#xf002;</div>
+				<input className="venues-search" id="venuesSearchBar" disabled={props.loading} onKeyPress={props.onEnter} placeholder="Check another location" type="text" />
+				<div className="search-icon" onClick={props.onYelpSearch}>&#xf002;</div>
 			</div>
 			<h1 className="venues-header">Venues</h1>
 			<div className="venues-list">
@@ -25,7 +25,9 @@ function Venues(props) {
 
 Venues.propTypes = {
 	loading: PropTypes.bool.isRequired,
-	venueList: PropTypes.array.isRequired
+	venueList: PropTypes.array.isRequired,
+	onYelpSearch: PropTypes.func.isRequired,
+	onEnter: PropTypes.func.isRequired
 };
 
 module.exports = Venues;
--- src/client/containers/VenuesContainer.js
+++ src/client/containers/VenuesContainer.js
@@ -12,17 +12,42 @@ var VenuesContainer = React.createClass({
 	componentDidMount: function() {
 		axios.post('/yelpFetch/' + this.props.params.location)
 		.then(function(yelpRes) {
-			console.log(yelpRes);
 			this.setState({
 				loading: false,
 				venueList: yelpRes.data.businesses
 			});
 		}.bind(this));
 	},
+	handleYelpSearch: function(e) {		
+		if (!this.state.loading) {
+			this.setState({
+				loading: true,
+				venueList: []
+			});
+
+			var searchValue = document.getElementById('venuesSearchBar').value;
+			axios.post('/yelpFetch/' + searchValue)
+			.then(function(yelpRes) {
+				this.setState({
+					loading: false,
+					venueList: yelpRes.data.businesses
+				});
+			}.bind(this));
+		}
+	},
+	handleEnter: function(e) {
+		if (e.charCode === 13) {
+			this.handleYelpSearch();
+		}
+	},
 	render: function() {
 		return (
 			<div>				
-				<Venues venueList={this.state.venueList} loading={this.state.loading} />
+				<Venues 
+					venueList={this.state.venueList} 
+					loading={this.state.loading} 
+					onYelpSearch={this.handleYelpSearch} 
+					onEnter={this.handleEnter} />
 			</div>
 		)			
 	}
