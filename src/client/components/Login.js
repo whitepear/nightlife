1fc52be99d0aa83e8919c8a43d9af99cbeaa72a3
John Doe <webdevnorth@gmail.com>
2016-12-12 09:55:37 +0000

    added login functionality to client-side app

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -6,19 +6,29 @@ function Login(props) {
 		<div>
 			<div className="login-bg"></div>	
 			<h1 className="form-header">Login</h1>	
-			<form action="/login" method="POST">
+			<form action="/login" method="POST" onSubmit={props.onLoginSubmit}>
 				<div className="form-group">
 					<label htmlFor="login-username">Username:</label>
-	    		<input type="text" className="form-control" id="login-username" required />
+	    		<input type="text" className="form-control" id="loginUsername" onChange={props.onFieldChange} required />
 				</div>
 				<div className="form-group">
 			    <label htmlFor="login-password">Password:</label>
-			    <input type="password" className="form-control" id="login-password" required />
+			    <input type="password" className="form-control" id="loginPassword" onChange={props.onFieldChange} required />
 			  </div>
-				<button type="submit" className="btn btn-default form-submit">Submit</button>
+			  <div className={(props.validationMessage ? 'show ' : '') + "registration-message"}>
+					{props.validationMessage}
+				</div>
+				<button type="submit" className="btn btn-default form-submit" disabled={props.validationPassed}>Submit</button>
 			</form>
 		</div>
 	)
 }
+
+Login.propTypes = {
+	onFieldChange: PropTypes.func.isRequired,
+	onLoginSubmit: PropTypes.func.isRequired,
+	validationMessage: PropTypes.string.isRequired,
+	validationPassed: PropTypes.bool.isRequired
+};
 
 module.exports = Login;
--- src/client/containers/LoginContainer.js
+++ src/client/containers/LoginContainer.js
@@ -1,9 +1,75 @@
 var React = require('react');
 var Login = require('../components/Login.js');
+var loginValidation = require('../utils/clientValidation.js').loginValidation;
+var axios = require('axios');
 
 var LoginContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function() {
+		return {
+			loginUsername: '',
+			loginPassword: '',
+			validationMessage: '',
+			validationPassed: false
+		};
+	},
+	handleFieldChange: function(e) {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	},
+	handleLoginSubmit: function(e) {
+		e.preventDefault();
+		var prevPath = this.props.location.query.prevPath;
+
+		var validationResult = loginValidation(this.state);
+		if (validationResult.validationPassed) {			
+			this.setState({
+				validationPassed: true
+			}, function() {
+				// client-side validation passed, pass to server for further validation
+				axios.post('/login', this.state)
+				.then(function(res) {
+					if (res.data.serverValidationPassed) {
+						// validation passed, now logged in
+						// redirect user
+						if (prevPath === undefined || prevPath === '/register') {
+							this.context.router.push('/');
+						} else {
+							this.context.router.push(prevPath);
+						}	
+					} else {
+						// validation failed, not logged in
+						this.setState({
+							validationMessage: res.data.serverValidationMessage,
+							validationPassed: false
+						});
+					}
+				}.bind(this)) // END.then
+				.catch(function(err) {
+					// catch axios post errors
+					console.log(err);
+					this.setState({
+						validationMessage: 'A server error occurred while processing your request. Please try again later.',
+						validationPassed: false
+					});
+				}.bind(this)); // END.catch
+			}.bind(this)); // END.setState callback
+		} else {
+			// client-side validation failed, update validation message for user on form
+			this.setState({				
+				validationMessage: validationResult.validationMessage
+			});
+		}
+	},
 	render: function() {
-		return <Login />
+		return <Login 
+						onFieldChange={this.handleFieldChange}
+						onLoginSubmit={this.handleLoginSubmit}
+						validationMessage={this.state.validationMessage}
+						validationPassed={this.state.validationPassed} />
 	}
 });
 
