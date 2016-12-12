0e00b50f005cde6b096828f0f3c136cdf6a0a675
John Doe <webdevnorth@gmail.com>
2016-12-12 00:18:23 +0000

    added login validation to serverValidation.js

--- src/server/utils/serverValidation.js
+++ src/server/utils/serverValidation.js
@@ -1,3 +1,5 @@
+var bcrypt = require('bcryptjs');
+
 // Validate registration form data
 module.exports.registrationValidation = function(userInfo, db, callback) {
 
@@ -49,7 +51,43 @@ module.exports.registrationValidation = function(userInfo, db, callback) {
 		}
 	})
 	.catch(function(err) {
-		console.log('Username/email database check error: ', err);
+		console.log('Registration database check error: ', err);
 		return callback(false, 'A server error occurred while attempting to validate your information.\nPlease try again later.');
 	});	
+}
+
+// validate login details
+module.exports.loginValidation = function(userInfo, db, callback) {
+	var username = userInfo.username;
+	var password = userInfo.password;
+
+	if(!username || !password) {
+		return callback(false, 'Please fill out all fields before submitting.');
+	}
+
+	// check for userDoc via username	
+	db.collection('users').findOne({ username: username })
+	.then(function(userDoc) {
+		if (userDoc === null) {
+			return callback(false, 'That username does not belong to a registered user.');
+		} else {
+			// check passwords match
+			bcrypt.compare(password, userDoc.password, function(err, result) {
+				if (err) {
+					console.log('An error occurred during bcrypt comparison: ', err);
+					return callback(false, 'A server error occurred while attempting to validate your information.\nPlease try again later.');
+				}
+
+				if (result) {
+					return callback(true, userDoc._id);
+				} else {
+					return callback(false, 'The password you have provided is incorrect.');
+				}
+			});
+		}
+	})
+	.catch(function(err) {
+		console.log('Login database check error: ', err);
+		return callback(false, 'A server error occurred while attempting to validate your information.\nPlease try again later.');
+	});
 }
